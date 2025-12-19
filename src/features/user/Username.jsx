b39b6029a1a06be135f3567b20e3a7be55360d9a
BogdanSFG <bogdan.sfg@gmail.com>
2025-12-19 22:00:57 +0200

    installed tailwind and added some classes

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <p className="hidden text-sm font-semibold md:block">Ionatan</p>;
+}
+
+export default Username;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,19 +1,22 @@
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "./Loader";
+import Header from './Header';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Loader from './Loader';
 function AppLayout() {
   const navigation = useNavigation();
 
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
 
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
       <Header></Header>
-      <main>
-        <Outlet></Outlet>
-      </main>
+      <div className="overflow-y-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet></Outlet>
+        </main>
+      </div>
+
       <CartOverview></CartOverview>
     </div>
   );
